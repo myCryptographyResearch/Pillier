68386c03cfaf3fabcaf846430a51b4193d9f1fed
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-09-22 19:30:57 +0330

    initialize Pillier key_gen

--- mthods.py
+++ mthods.py
@@ -1,4 +1,5 @@
-
+import math
+import random
 class Pillier:
     def __init__(self):
         print("Pillier cryptographi cinstructore is called...")
@@ -9,6 +10,10 @@ class Pillier:
         self.q = 7
         self.n = self.p * self.q
         self.phi = (self.p - 1) * (self.q - 1)
+
+        self.g = random.randint(0, self.n * self.n)
+        self.lmbda = math.lcm(self.p - 1, self.q - 1)
+        self.mu = pow(self.lx(pow(self.g, self.lmbda, self.n * self.n)), -1, self.n)
 
     def lx(self, x):
         y = (x - 1) / self.n
