d813902ea8f25299f620ec1776026885772d67a7
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-09-22 19:29:04 +0330

    initialize Pillier key_gen

--- mthods.py
+++ mthods.py
@@ -3,6 +3,12 @@ class Pillier:
     def __init__(self):
         print("Pillier cryptographi cinstructore is called...")
 
+    def key_gen(self):
+        self.p = 13
+        self.q = 7
+        self.n = self.p * self.q
+        self.phi = (self.p - 1) * (self.q - 1)
+
 
 
 class Server:
