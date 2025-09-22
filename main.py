fb8a9be9a62b5568176e57ff04bdf4cf066b4ca3
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-09-22 20:14:02 +0330

    initialize Clientnetwork

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from mthods import Server, Client
-import random
 
-"""cryptographer = Pillier()
-
-m = 123
-n = 13*7
-r = random.randint(0, n)
-
-c = cryptographer.encrypt(m, r)
-p = cryptographer.decrypt(c)
-
-print(c)
-print(p)"""
 
 
 server1 = Server("localhost")
@@ -21,3 +9,11 @@ if type(server1).__name__ == 'Server':
 client1 = Client("Telescope1", server1)
 client2 = Client("Telescope2", server1)
 client3 = Client("Telescope3", server1)
+
+m = 123
+c = client1.encryptor(m)
+p = server1.decryptor(c)
+
+print(m)
+print(c)
+print(p)
